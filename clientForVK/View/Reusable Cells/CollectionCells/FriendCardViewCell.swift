892e5d34a12e2724329b6b17a8a41bdbdce37a9b
nolevvel <nick_trushin@icloud.com>
2022-01-20 22:01:46 +0600

    организовал по другому способ установки лайков на фото
    сорри очень отстал от программы тут проверять от прошлого сильно нечего. сменил только лайки. в следующем пул реквесте все нагоню

--- clientForVK/View/Reusable Cells/CollectionCells/FriendCardViewCell.swift
+++ clientForVK/View/Reusable Cells/CollectionCells/FriendCardViewCell.swift
@@ -9,17 +9,30 @@ import UIKit
 
 final class FriendCardViewCell: UICollectionViewCell {
     @IBOutlet var friendPhoto: UIImageView!
-    @IBOutlet public var likeButton: UIButton!
+    @IBOutlet var likeButton: UIButton!
+
+    @IBAction func setLikeFor() {
+        if likeButton.isSelected {
+            likeButton.isSelected = false
+            likeButton.tintColor = .gray
+        } else {
+            likeButton.isSelected = true
+            likeButton.tintColor = .red
+        }
+    }
     
     func configure(photo: UIImage) {
         self.friendPhoto.image = photo
+        
+        likeButton.tintColor = .gray
+        likeButton.setImage(UIImage(systemName: "heart") ?? UIImage(), for: .normal)
+        likeButton.setImage(UIImage(systemName: "heart.fill") ?? UIImage(), for: .selected)
+        likeButton.setTitle("1", for: .selected)
+        likeButton.setTitle("0", for: .normal)
     }
-    
-   
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
 }
